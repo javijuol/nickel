2cfde42dfe2facb5acce7e336c51aeb7203272db
steve <steve@promivia.com>
2015-02-24 16:47:33 +0100

    Added find_name method to wrapper

--- lib/holidays_wrapper.rb
+++ lib/holidays_wrapper.rb
@@ -1,3 +1,12 @@
 class HolidaysWrapper < Holidays
-
+  # sm - reads every holiday in region, including informal, and returns a date or nil
+  # the date range is for the 12 month period following now
+  def self.find_name(region, name)
+    Holidays.between(Date.today,Date.today>>12, region, :informal).each do |htest|
+      if htest[:name] == name
+        return htest[:date]
+      end
+    end
+    return nil
+  end
 end
